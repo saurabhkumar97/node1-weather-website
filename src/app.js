33075005fdb0631f3b4ca4e3b26c80822b9eb024
Saurabh kumar <saurabhkumar2505@gmail.com>
2020-05-02 21:53:36 +0530

    changes in title font

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('',(req, res) => {
     res.render('index', {
-        title: 'weather App',
+        title: 'Weather App',
         name: 'Saurabh'
     })
 })
